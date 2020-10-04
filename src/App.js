71030e900a8ca5da74a11cdc3ca46abd238896ea
Wrushasen Dakhane <wrushu2004@gmail.com>
2020-10-04 09:02:27 -0700

    React Use Custom Hook implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Container } from "react-bootstrap"
+import useFetchJobs from './hooks/useFetchJobs';
+import Job from './containers/Job';
+import JobPagination from './containers/JobPagination';
+import SearchForm from './components/SearchForm';
 
 function App() {
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
+
+  const handleParamChange = ({ target }) => {
+    setPage(1);
+    setParams(prevParams => {
+      return { ...prevParams, [target.name]: target.value }
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="my-4">
+      <h1 className="mb-4">GitHub Jobs</h1>
+      <SearchForm params={params} onParamChange={handleParamChange} />
+      { loading && <h1>Loading...</h1>}
+      { error && <h1>Error. Try Refreshing.</h1>}
+      { jobs.length > 0 ?
+        <div>
+          <JobPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+          {jobs.map(job => <Job key={job.id} job={job} />)}
+          <JobPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+        </div> : null
+      }
+    </Container >
   );
 }
 
---
+++ src/hooks/useFetchJobs.jsx
@@ -0,0 +1,110 @@
+import { useEffect, useReducer } from "react";
+import axios from "axios";
+axios.defaults.baseURL =
+  "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json";
+const INITIAL_STATE = {
+  jobs: [],
+  loading: false,
+  error: null,
+};
+
+const actions = {
+  FETCH_JOBS_START: "FETCH_JOBS_START",
+  FETCH_JOBS_SUCCESS: "FETCH_JOBS_SUCCESS",
+  FETCH_JOBS_FAILED: "FETCH_JOBS_FAILED",
+  UPDATE_HAS_NEXT_PAGE: "UPDATE_HAS_NEXT_PAGE",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case actions.FETCH_JOBS_START:
+      return { ...state, loading: true, error: null };
+    case actions.FETCH_JOBS_SUCCESS:
+      return {
+        ...state,
+        jobs: action.payload.jobs,
+        loading: false,
+        error: null,
+      };
+    case actions.FETCH_JOBS_FAILED:
+      return {
+        ...state,
+        jobs: [],
+        loading: false,
+        error: action.payload.error,
+      };
+    case actions.UPDATE_HAS_NEXT_PAGE:
+      return {
+        ...state,
+        hasNextPage: action.payload.hasNextPage,
+      };
+    default:
+      return state;
+  }
+};
+
+const useFetchJobs = (params, page) => {
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+
+  useEffect(() => {
+    const cancelToken1 = axios.CancelToken;
+    const source1 = cancelToken1.source();
+
+    dispatch({ type: actions.FETCH_JOBS_START });
+    axios
+      .get("", {
+        cancelToken1: source1.token,
+        params: { markdown: true, page: page, ...params },
+      })
+      .then((response) =>
+        dispatch({
+          type: actions.FETCH_JOBS_SUCCESS,
+          payload: { jobs: response.data },
+        })
+      )
+      .catch((error) => {
+        if (axios.isCancel(error)) return;
+        dispatch({
+          type: actions.FETCH_JOBS_FAILED,
+          payload: { error: error },
+        });
+      });
+
+    const cancelToken2 = axios.CancelToken;
+    const source2 = cancelToken2.source();
+
+    dispatch({ type: actions.FETCH_JOBS_START });
+    axios
+      .get("", {
+        cancelToken2: source2.token,
+        params: { markdown: true, page: page + 1, ...params },
+      })
+      .then((response) =>
+        dispatch({
+          type: actions.UPDATE_HAS_NEXT_PAGE,
+          payload: { hasNextPage: response.data.length !== 0 },
+        })
+      )
+      .catch((error) => {
+        if (axios.isCancel(error)) return;
+        dispatch({
+          type: actions.FETCH_JOBS_FAILED,
+          payload: { error: error },
+        });
+      });
+
+    return () => {
+      source1.cancel();
+      source2.cancel();
+    };
+  }, [params, page]);
+
+  return state;
+};
+
+export default useFetchJobs;
+
+// link: <https://api.github.com/search/code?q=addClass+user%3Amozilla&page=13>; rel="prev",
+// <https://api.github.com/search/code?q=addClass+user%3Amozilla&page=15>; rel="next",
+// <https://api.github.com/search/code?q=addClass+user%3Amozilla&page=34>; rel="last",
+// <https://api.github.com/search/code?q=addClass+user%3Amozilla&page=1>; rel="first"
