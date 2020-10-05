fb11a7fb7b380a7e493095741bceffb804e94164
Wrushasen Dakhane <wrushu2004@gmail.com>
2020-10-04 17:36:31 -0700

    Added Search Button and removed rendering on input change

--- src/App.js
+++ src/App.js
@@ -6,20 +6,26 @@ import JobPagination from './containers/JobPagination';
 import SearchForm from './components/SearchForm';
 
 function App() {
+  const [inputParams, setInputParams] = useState({})
   const [params, setParams] = useState({})
   const [page, setPage] = useState(1)
   const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
 
   const handleParamChange = ({ target }) => {
     setPage(1);
-    setParams(prevParams => {
+    setInputParams(prevParams => {
       return { ...prevParams, [target.name]: target.value }
     })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setParams({ ...inputParams });
   }
   return (
     <Container className="my-4">
       <h1 className="mb-4">GitHub Jobs</h1>
-      <SearchForm params={params} onParamChange={handleParamChange} />
+      <SearchForm params={inputParams} onParamChange={handleParamChange} onSearch={handleSubmit} />
       { loading && <h1>Loading...</h1>}
       { error && <h1>Error. Try Refreshing.</h1>}
       { jobs.length > 0 ?
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,26 +1,28 @@
 import React from "react";
-import { Col, Form } from "react-bootstrap";
+import { Col, Form, Button } from "react-bootstrap";
 
-function SearchForm({ params, onParamChange }) {
+function SearchForm({ params, onParamChange, onSearch }) {
   return (
-    <Form className="mb-4">
+    <Form className="mb-4" onSubmit={onSearch}>
       <Form.Row className="align-items-end">
-        <Form.Group as={Col}>
+        <Form.Group as={Col} sm="12" md="4">
           <Form.Label>Description</Form.Label>
           <Form.Control
             onChange={onParamChange}
             value={params.description}
             name="description"
             type="text"
+            placeholder="Filter by title, benefits, companies, expertise"
           />
         </Form.Group>
-        <Form.Group as={Col}>
+        <Form.Group as={Col} sm="12" md="4">
           <Form.Label>Location</Form.Label>
           <Form.Control
             onChange={onParamChange}
             value={params.location}
             name="location"
             type="text"
+            placeholder="Filter by city, state, zip code or country"
           />
         </Form.Group>
         <Form.Group as={Col} xs="auto" className="ml-2">
@@ -33,6 +35,11 @@ function SearchForm({ params, onParamChange }) {
             type="checkbox"
             className="mb-2"
           />
+        </Form.Group>
+        <Form.Group as={Col} xs="auto" className="ml-2">
+          <Button variant="primary" type="submit">
+            Search
+          </Button>
         </Form.Group>
       </Form.Row>
     </Form>
